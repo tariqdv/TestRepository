a2673115307ff700c687b6b9b1d2921bbf83dc44
MrZebarth <kris.zebarth@ocsb.ca>
2023-11-23 10:24:42 -0500

    I'm Batman

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wow! I can do stuff!");
+		while (true) {
+			System.out.println("I'm Batman!");
+		}
 	}
 
 }
